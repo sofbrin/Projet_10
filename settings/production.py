df1f4715c4fe169e879b7861ee4668d7fca78400
sofbrin <sofbrin@hotmail.com>
2020-05-04 22:10:14 +0200

    update settings

---
+++ settings/production.py
@@ -0,0 +1,17 @@
+from .development import *
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = False
+ALLOWED_HOSTS = ['51.210.4.42']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('db_name'),
+        'USER': os.environ.get('db_user'),
+        'PASSWORD': os.environ.get('db_password'),
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
